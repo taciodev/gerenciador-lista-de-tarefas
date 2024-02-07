76fc43daf69af6c5f94e352c7ce92db7dabf602e
Taciano da Hora <taciano.dev@outlook.com>
2024-02-07 13:52:31 -0300

    feat: add initial task manager menu

---
+++ main.py
@@ -0,0 +1,32 @@
+while True:
+    print("\n*********************************************")
+    print("*          Menu de Lista de Tarefas         *")
+    print("*-------------------------------------------*")
+    print("* 1. Adicionar tarefa                      *")
+    print("* 2. Ver tarefas                           *")
+    print("* 3. Atualizar tarefa                      *")
+    print("* 4. Completar tarefa                      *")
+    print("* 5. Deletar tarefas completadas           *")
+    print("* 6. Sair                                  *")
+    print("*********************************************")
+
+    choice = input("\nQual a sua escolha: ")
+
+    if choice == "1":
+        ...
+    elif choice == "2":
+        ...
+    elif choice == "3":
+        ...
+    elif choice == "4":
+        ...
+    elif choice == "5":
+        ...
+    elif choice == "6":
+        print("Saindo...")
+        break
+    else:
+        print("Opção inválida. Por favor, escolha uma opção válida.")
+
+
+print("Programa finalizado com sucesso!")
